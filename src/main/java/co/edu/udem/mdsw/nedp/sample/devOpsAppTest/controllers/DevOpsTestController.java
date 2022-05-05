b80e4a93f006de059de411cf822d56d5cd1aebee
jramirezl <>
2022-05-05 08:41:51 -0500

    Fix Thymeleaf

--- src/main/java/co/edu/udem/mdsw/nedp/sample/devOpsAppTest/controllers/DevOpsTestController.java
+++ src/main/java/co/edu/udem/mdsw/nedp/sample/devOpsAppTest/controllers/DevOpsTestController.java
@@ -1,8 +1,6 @@
 package co.edu.udem.mdsw.nedp.sample.devOpsAppTest.controllers;
 
 
-import co.edu.udem.mdsw.nedp.sample.devOpsAppTest.service.ManejoUsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
